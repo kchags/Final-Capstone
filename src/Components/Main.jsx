ec57c87be5601b4bed47c97c5d70a30956e759e9
Kevin Chagas <kevinchagas@yahoo.comKevinKevin>
2023-07-12 14:46:08 -0500

    moving commented code to bottom

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,3 +1,91 @@
+import React, { useEffect, useState } from "react";
+import Card from "./Card";
+import Pokeinfo from "./Pokeinfo";
+import axios from "axios";
+import SearchBar from "./SearchBar";
+import SearchResultsLists from "./SearchResultsLists";
+import useHistory from "react-router-dom"; // Import useHistory from react-router-dom
+
+const Main = () => {
+  const [pokeData, setPokeData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [url, setUrl] = useState("https://pokeapi.co/api/v2/pokemon/");
+  const [nextUrl, setNextUrl] = useState();
+  const [prevUrl, setPrevUrl] = useState();
+  const [pokeDex, setPokeDex] = useState();
+  const [searchResults, setSearchResults] = useState([]);
+  const history = useHistory(); // Initialize useHistory
+
+  const pokeFun = async () => {
+    setLoading(true);
+    const res = await axios.get(url);
+    setNextUrl(res.data.next);
+    setPrevUrl(res.data.previous);
+    getPokemon(res.data.results);
+    setLoading(false);
+  };
+
+  const getPokemon = async (res) => {
+    await Promise.all(
+      res.map(async (item) => {
+        const result = await axios.get(item.url);
+        setPokeData((state) => {
+          state = [...state, result.data];
+          state.sort((a, b) => (a.id > b.id ? 1 : -1));
+          return state;
+        });
+      })
+    );
+  };
+
+  useEffect(() => {
+    pokeFun();
+  }, [url]);
+
+  const handleSearch = (value) => {
+    const filteredResults = pokeData.filter((pokemon) =>
+      pokemon.name.toLowerCase().includes(value.toLowerCase())
+    );
+    setSearchResults(filteredResults);
+  };
+
+  const handleSelectPokemon = (pokemon) => {
+    history.push(`/pokemon/${pokemon.name}`); // Redirect to the selected Pokémon's card
+  };
+
+  return (
+    <>
+      <div className="container">
+        <div className="left-content">
+          <SearchBar setResults={setSearchResults} /> {/* Pass setResults function as a prop */}
+          {searchResults.length > 0 ? (
+            <SearchResultsLists
+              results={searchResults}
+              onSelectPokemon={handleSelectPokemon} // Pass onSelectPokemon function as a prop
+            />
+          ) : (
+            <Card pokemon={pokeData} loading={loading} infoPokemon={setPokeDex} />
+          )}
+
+          <div className="btn-group">
+            {prevUrl && (
+              <button onClick={() => setUrl(prevUrl)}>Previous</button>
+            )}
+            {nextUrl && <button onClick={() => setUrl(nextUrl)}>Next</button>}
+          </div>
+        </div>
+        <div className="right-content">
+          <Pokeinfo data={pokeDex} />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Main;
+
+
+
 // import React, { useEffect, useState } from "react";
 // import Card from "./Card";
 // import Pokeinfo from "./Pokeinfo";
@@ -158,88 +246,3 @@
 
 
 
-import React, { useEffect, useState } from "react";
-import Card from "./Card";
-import Pokeinfo from "./Pokeinfo";
-import axios from "axios";
-import SearchBar from "./SearchBar";
-import SearchResultsLists from "./SearchResultsLists";
-import useHistory from "react-router-dom"; // Import useHistory from react-router-dom
-
-const Main = () => {
-  const [pokeData, setPokeData] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [url, setUrl] = useState("https://pokeapi.co/api/v2/pokemon/");
-  const [nextUrl, setNextUrl] = useState();
-  const [prevUrl, setPrevUrl] = useState();
-  const [pokeDex, setPokeDex] = useState();
-  const [searchResults, setSearchResults] = useState([]);
-  const history = useHistory(); // Initialize useHistory
-
-  const pokeFun = async () => {
-    setLoading(true);
-    const res = await axios.get(url);
-    setNextUrl(res.data.next);
-    setPrevUrl(res.data.previous);
-    getPokemon(res.data.results);
-    setLoading(false);
-  };
-
-  const getPokemon = async (res) => {
-    await Promise.all(
-      res.map(async (item) => {
-        const result = await axios.get(item.url);
-        setPokeData((state) => {
-          state = [...state, result.data];
-          state.sort((a, b) => (a.id > b.id ? 1 : -1));
-          return state;
-        });
-      })
-    );
-  };
-
-  useEffect(() => {
-    pokeFun();
-  }, [url]);
-
-  const handleSearch = (value) => {
-    const filteredResults = pokeData.filter((pokemon) =>
-      pokemon.name.toLowerCase().includes(value.toLowerCase())
-    );
-    setSearchResults(filteredResults);
-  };
-
-  const handleSelectPokemon = (pokemon) => {
-    history.push(`/pokemon/${pokemon.name}`); // Redirect to the selected Pokémon's card
-  };
-
-  return (
-    <>
-      <div className="container">
-        <div className="left-content">
-          <SearchBar setResults={setSearchResults} /> {/* Pass setResults function as a prop */}
-          {searchResults.length > 0 ? (
-            <SearchResultsLists
-              results={searchResults}
-              onSelectPokemon={handleSelectPokemon} // Pass onSelectPokemon function as a prop
-            />
-          ) : (
-            <Card pokemon={pokeData} loading={loading} infoPokemon={setPokeDex} />
-          )}
-
-          <div className="btn-group">
-            {prevUrl && (
-              <button onClick={() => setUrl(prevUrl)}>Previous</button>
-            )}
-            {nextUrl && <button onClick={() => setUrl(nextUrl)}>Next</button>}
-          </div>
-        </div>
-        <div className="right-content">
-          <Pokeinfo data={pokeDex} />
-        </div>
-      </div>
-    </>
-  );
-};
-
-export default Main;
--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -1,3 +1,47 @@
+import React, { useState } from "react";
+import { FaSearch } from "react-icons/fa";
+import "./SearchBar.css"
+
+const SearchBar = ({ setResults }) => { // Accept setResults as a prop
+  const [input, setInput] = useState("");
+
+  const handleChange = (value) => {
+    setInput(value);
+    setResults([]); // Reset the results
+    fetchData(value);
+  };
+
+  const fetchData = (value) => {
+    fetch("https://pokeapi.co/api/v2/pokemon/")
+      .then((response) => response.json())
+      .then((json) => {
+        const results = json.results.filter((pokemon) =>
+          pokemon.name.toLowerCase().includes(value.toLowerCase())
+        );
+        setResults(results);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+        setResults([]); // Reset the results in case of an error
+      });
+  };
+
+  return (
+    <div className="input-wrapper">
+      <FaSearch id="search-icon" />
+      <input
+        placeholder="Type to search..."
+        value={input}
+        onChange={(e) => handleChange(e.target.value)}
+      />
+    </div>
+  );
+};
+
+export default SearchBar;
+
+
+
 // import { useState } from "react";
 // import { FaSearch } from "react-icons/fa";
 // import "./SearchBar.css";
@@ -54,44 +98,3 @@
 //   );
 // };
 
-import React, { useState } from "react";
-import { FaSearch } from "react-icons/fa";
-import "./SearchBar.css"
-
-const SearchBar = ({ setResults }) => { // Accept setResults as a prop
-  const [input, setInput] = useState("");
-
-  const handleChange = (value) => {
-    setInput(value);
-    setResults([]); // Reset the results
-    fetchData(value);
-  };
-
-  const fetchData = (value) => {
-    fetch("https://pokeapi.co/api/v2/pokemon/")
-      .then((response) => response.json())
-      .then((json) => {
-        const results = json.results.filter((pokemon) =>
-          pokemon.name.toLowerCase().includes(value.toLowerCase())
-        );
-        setResults(results);
-      })
-      .catch((error) => {
-        console.error("Error fetching data:", error);
-        setResults([]); // Reset the results in case of an error
-      });
-  };
-
-  return (
-    <div className="input-wrapper">
-      <FaSearch id="search-icon" />
-      <input
-        placeholder="Type to search..."
-        value={input}
-        onChange={(e) => handleChange(e.target.value)}
-      />
-    </div>
-  );
-};
-
-export default SearchBar;
